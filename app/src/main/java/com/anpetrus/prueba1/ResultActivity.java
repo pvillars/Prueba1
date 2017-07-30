509d4b7c94d168eb3a1dc7bd76753ed70b1c9a52
Petrus <pvillar@hotmail.cl>
2017-07-29 21:30:16 -0400

    final sin valoers en variables en xmls

--- app/src/main/java/com/anpetrus/prueba1/ResultActivity.java
+++ app/src/main/java/com/anpetrus/prueba1/ResultActivity.java
@@ -1,13 +1,22 @@
 package com.anpetrus.prueba1;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -17,28 +26,67 @@ public class ResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_result);
 
         Intent intent = getIntent();
-        String nameIn = intent.getStringExtra("NAME").toUpperCase();
+        final String nameIn = intent.getStringExtra("NAME").toUpperCase().trim();
         boolean genderMale = intent.getBooleanExtra("GENDER_MALE", true); //Default true
 
-        ImageView imageResult = (ImageView) findViewById(R.id.imageResult);
+        final ImageView imageResult = (ImageView) findViewById(R.id.imageResult);
         TextView textResult = (TextView) findViewById(R.id.textResult);
 
         if (genderMale) {
-            textResult.setText(nameIn.trim() + ", eres el mejor, SALUD!!!");
+            textResult.setText(nameIn + ", eres el mejor, SALUD!!!");
             imageResult.setImageResource(R.mipmap.beer);
         } else {
-            textResult.setText(nameIn.trim() + ", eres la mejor y la más hermoza!!!");
+            textResult.setText(nameIn + ", eres la mejor y la más hermoza!!!");
             imageResult.setImageResource(R.mipmap.flower);
         }
-
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.sendImage);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Mostrar opciones de envio de imagen", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // imageResult.setImageBitmap(getImage());
+                sharePic(getImage(),nameIn , "Mejorando el día");
+
             }
         });
+    }
+
+    public Bitmap getImage() {
+        LinearLayout shareLayout = (LinearLayout) this.findViewById(R.id.contentResultLayout);
+        shareLayout.setDrawingCacheEnabled(true);
+        shareLayout.buildDrawingCache();
+        Bitmap bm = shareLayout.getDrawingCache();
+        // ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        //bm.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        return bm;
+    }
+
+    private void sharePic(Bitmap image, String nameImage, String message) {
+
+        //Se guarda la imagen en la SDCARD
+        File f;
+        try {
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            image.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+
+            f = new File(Environment.getExternalStorageDirectory() + File.separator +
+                     File.separator + nameImage +".jpg");
+
+            f.createNewFile();
+
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(bytes.toByteArray());
+            bytes.flush();
+            bytes.close();
+            //compartir imagen
+            Intent share = new Intent(Intent.ACTION_SEND);
+            share.setType("image/jpeg");
+            share.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(f));
+            share.putExtra(Intent.EXTRA_TEXT, message);
+            startActivity(Intent.createChooser(share, "Compartir imagen"));
+        } catch (IOException e) {
+            Log.e("ERROR", e.getMessage());
+        }
+
     }
 }
